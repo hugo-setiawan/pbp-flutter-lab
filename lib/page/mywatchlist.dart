cc0fe0855128748e2cb19f6e79a4823154b74d6b
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-11-23 00:02:01 +0700

    Initially implement mywatchlist page and watchlist detail page

--- lib/page/mywatchlist.dart
+++ lib/page/mywatchlist.dart
@@ -1,30 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+import 'package:counter_7/page/watchlist_detail.dart';
 import 'package:counter_7/widget/drawer.dart';
+import 'package:counter_7/model/mywatchlist_model.dart';
 
 class MyWatchlistPage extends StatefulWidget {
-    const MyWatchlistPage({super.key});
+  const MyWatchlistPage({Key? key}) : super(key: key);
 
-    @override
-    State<MyWatchlistPage> createState() => _MyWatchlistPageState();
+  @override
+  State<MyWatchlistPage> createState() => _MyWatchlistPageState();
 }
 
 class _MyWatchlistPageState extends State<MyWatchlistPage> {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: const Text('My Watch List'),
-            ),
-            drawer: const MyDrawer(),
-            body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const <Widget>[
-                        Text('TODO!'),
-                    ],
-                ),
-            ),
-        );
+  Future<List<Watchlist>> fetchMyWatchlist() async {
+    var url =
+        Uri.parse('https://pbp-tugas-hugo.herokuapp.com/mywatchlist/json/');
+
+    var resp = await http.get(url, headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    });
+
+    var data = jsonDecode(utf8.decode(resp.bodyBytes));
+
+    List<Watchlist> listMyWatchlist = [];
+    for (var each in data) {
+      if (each != null) {
+        listMyWatchlist.add(Watchlist.fromJson(each));
+      }
     }
+
+    return listMyWatchlist;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('My Watch List'),
+        ),
+        drawer: const MyDrawer(),
+        body: FutureBuilder(
+          future: fetchMyWatchlist(),
+          builder: (context, snapshot) {
+            if (snapshot.data == null) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (!snapshot.hasData) {
+              return Column(
+                children: const [
+                  Text(
+                    "Belum ada isi Watch List :(",
+                    style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                  ),
+                  SizedBox(height: 8),
+                ],
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) {
+                          return WatchlistDetailPage(
+                              watchlist: snapshot.data![index]);
+                        },
+                      ));
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                      padding: const EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15.0),
+                        boxShadow: [
+                          BoxShadow(
+                            // warna berubah sesuai status watched
+                            color: snapshot.data![index].fields.watched ? Colors.green : Colors.red,
+                            blurRadius: 5
+                          )
+                        ]
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            snapshot.data![index].fields.title,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ), // TODO add checkbox watched status
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ));
+  }
 }
